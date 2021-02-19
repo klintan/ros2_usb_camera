3326b9bd11dd8300ea708fabc77fb257552fe639
Alexander Gutenkunst <alex.gutenkunst+github@gmail.com>
2021-02-19 05:22:11 +0100

    Foxy devel adaptions (#12)
    * Use OpenCV 4
    
    * Fix deprecated header includes
    
    * Simplify launch files / Install config
    
    * Use industrial_ci
    
    * Add missing test-dep on amend_lint_auto
    
    Co-authored-by: Alexander Gutenkunst <a.gutenkunst+github@gmail.com>

--- include/usb_camera_driver.hpp
+++ include/usb_camera_driver.hpp
@@ -34,8 +34,8 @@ SOFTWARE.
 
 #include "sensor_msgs/image_encodings.hpp"
 
-#include <camera_info_manager/camera_info_manager.h>
-#include <image_transport/image_transport.h>
+#include <camera_info_manager/camera_info_manager.hpp>
+#include <image_transport/image_transport.hpp>
 
 #include "opencv2/highgui/highgui.hpp"
 #include <opencv2/imgproc.hpp>
